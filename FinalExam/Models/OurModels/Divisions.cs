18d38620f5c6d826ea6ce23699ffffbb8b921927
joyboi616 <200455316@student.georgianc.on.ca>
2021-12-13 18:31:57 -0500

    Agregar archivos de proyecto.

---
+++ FinalExam/Models/OurModels/Divisions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace FinalExam.Models.OurModels
+{
+    public class Divisions
+    {
+    }
+}
